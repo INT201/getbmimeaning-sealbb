9768531ad26db7df8c6879918bc5571fe91129f6
Chonticha <Chonticha.it@mail.kmutt.ac.th>
2022-09-10 01:58:58 +0700

    finish

--- getBMIMeaning.js
+++ getBMIMeaning.js
@@ -2,8 +2,17 @@ const { template } = require('@babel/core')
 
 function calculateBMI(weight, height) {
   //code here
+  return weight/(height*height)
 }
 function getBMIMeaning(weight, height) {
   //code here
+  let BMI=calculateBMI(weight,height)
+  if(BMI<18.5){
+    return 'Underweight'
+  }else if(BMI>=18.5&&BMI<=24.9){
+    return 'Normal weight'
+  }else{
+    return 'Overweight'
+  }
 }
 module.exports = getBMIMeaning
